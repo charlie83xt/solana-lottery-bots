541024e3eb141efebe12776f0ff48acf124efa01
charlie83xt <charlie83xt@hotmail.com>
2025-04-16 18:12:28 +0100

    google service onto base64

--- bots/claim-sheet.js
+++ bots/claim-sheet.js
@@ -2,7 +2,9 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 async function logClaimToSheet({ wallet, amount, lotteryId, tx, role }) {
-    const creds = JSON.parse(process.env.GOOGLE_SERVICE_JSON);
+    const creds = JSON.parse(
+      Buffer.from(process.env.GOOGLE_SERVICE_JSON_B64, 'base64').toString('utf8')
+    );
     const doc = new GoogleSpreadsheet('1cANnNd5Mn0pelmdrOuR__EtYb1hV5mme4mrcQhTzD2Y')
     await doc.useServiceAccountAuth(creds);
     await doc.loadInfo();
